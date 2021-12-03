3a2309f6be0b5ddcfecba34212017c0b6dcc0992
stazcp <staz.christo@gmail.com>
2021-12-02 17:42:44 -0800

    stage 2: markup tranlator working

--- src/components/MarkdownOutput.js
+++ src/components/MarkdownOutput.js
@@ -1,5 +1,13 @@
+import { useContext } from 'react'
 import { Container } from '@mui/material'
+import { AppContext } from '../state/appState'
+import ReactMarkdown from 'react-markdown'
 
 export default function MarkdownOutput() {
-  return <Container maxWidth="sm">asdsad</Container>
+  const { markedText } = useContext(AppContext)
+  return (
+    <Container maxWidth="sm">
+      <ReactMarkdown children={markedText} />
+    </Container>
+  )
 }
