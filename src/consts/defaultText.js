e0ac60e3526eb0905d2a02cd449c27e717d2c356
stazcp <staz.christo@gmail.com>
2021-12-02 19:04:58 -0800

    Alpha: basic functionality

--- src/consts/defaultText.js
+++ src/consts/defaultText.js
@@ -1,4 +1,8 @@
 export const defaultText = ` # React Markdown Editor 
   \n ### This is sample Markdown for demo purposes 
-  \n **how to be bold** \n \n *or speak italian* \n 
-   I have added a few hints on the bottom on some basic Markdown. `
+  \n - **how to be bold**
+  \n - *or speak italian*
+  \n 1. I have added a few hints on the bottom on some basic Markdown.
+  \n 2. Feel free to edit this text or add your own instead.
+  \n                      Enjoy
+   `
